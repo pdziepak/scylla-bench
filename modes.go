b9a73ed84dcb4014e24f292054587122af55c059
Dmitry Kropachev <dmitry.kropachev@gmail.com>
2021-06-07 17:32:32 +0300

    main.go, modes.go: make it register timeout errors

--- modes.go
+++ modes.go
@@ -261,19 +261,22 @@ func RunTest(resultChannel chan Result, workload WorkloadGenerator, rateLimiter 
 	for !iter.IsDone() {
 		rateLimiter.Wait()
 
-		start := rateLimiter.Expected()
-		if start.IsZero() {
-			start = time.Now()
+		expectedStartTime := rateLimiter.Expected()
+		if expectedStartTime.IsZero() {
+			expectedStartTime = time.Now()
 		}
 
-		err, _ := test(rb)
+		err, rawLatency := test(rb)
 		if err != nil {
-			log.Print(err)
 			rb.IncErrors()
-			continue
+			log.Print(err)
+			if rawLatency > errorToTimeoutCutoffTime {
+				// Consider this error to be timeout error and register it in histogram
+				rb.RecordLatency(start, time.Now())
+			}
+		} else {
+			rb.RecordLatency(start, time.Now())
 		}
-
-		rb.RecordLatency(start, time.Now())
 
 		now := time.Now()
 		if now.Sub(partialStart) > time.Second {
